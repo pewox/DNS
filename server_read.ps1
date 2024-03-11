b3d01c89c04b40ee980590aa34c870ec795f57f4
pewox <117673001+pewox@users.noreply.github.com>
2024-03-11 23:04:52 +0100

    Update server_read.ps1

--- server_read.ps1
+++ server_read.ps1
@@ -2,7 +2,7 @@ $path = '.\server.csv'
 $csv = Import-Csv -Path  $path
 $path_check = Test-Path -Path $path
 $csv_check = -not [string]::IsNullOrEmpty((Get-Content -Path $path))
-$lines = [int](Get-Content -Path $path | Measure-Object -Line).Lines
+$lines = [int](Get-Content -Path $path | Measure-Object -Line).Lines -1
 
 for($i = 0; $i -lt $lines; $i++){
     Write-Host $csv.old[$i]
